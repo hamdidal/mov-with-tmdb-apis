289b3ec8dc31267d6a5666918d6c3aa24a5021a0
icht4i <hamdidal@yahoo.com>
2024-04-28 16:39:36 +0300

    first commit

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -32,54 +32,51 @@ const Dashboard = () => {
 
   return (
     <div>
-      <Header />
-      {!isLoading ? (
-        <>
-          <Row gutter={[32, 16]}>
-            <div
-              onClick={() => navigate(`/details/${movies[0].id}`)}
-              className="first-movie"
-              style={{
-                backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0), #828276), url(https://image.tmdb.org/t/p/w1280${
-                  movies![0]?.backdrop_path
-                })`,
-              }}
-            >
-              <div className="first-movie-detail">
-                <Typography.Title className="film-title" level={3}>
-                  {movies[0]?.title}
-                </Typography.Title>
-                <Typography.Paragraph
-                  className="film-overview"
-                  ellipsis={{ rows: 3, expandable: true }}
-                >
-                  {movies[0]?.overview}
-                </Typography.Paragraph>
-                <Typography.Title className="film-release-date" level={5}>
-                  {convertDate(movies[0]?.release_date)}
-                </Typography.Title>
-              </div>
-            </div>
-          </Row>
-          <InfiniteScroll
-            dataLength={page * movies.length}
-            next={() => {
-              setPage(page + 1);
+      <Header />(
+      <>
+        <Row gutter={[32, 16]}>
+          <div
+            onClick={() => navigate(`/details/${movies[0].id}`)}
+            className="first-movie"
+            style={{
+              backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0), #828276), url(https://image.tmdb.org/t/p/w1280${
+                movies![0]?.backdrop_path
+              })`,
             }}
-            hasMore={true}
-            loader={true}
-            className="dashboard-cards"
           >
-            <Row className="dashboard-row" gutter={[32, 16]}>
-              {movies.slice(1).map((movie: MovieModel) => (
-                <ColComponent movie={movie} />
-              ))}
-            </Row>
-          </InfiniteScroll>
-        </>
-      ) : (
-        <Spinner />
-      )}
+            <div className="first-movie-detail">
+              <Typography.Title className="film-title" level={3}>
+                {movies[0]?.title}
+              </Typography.Title>
+              <Typography.Paragraph
+                className="film-overview"
+                ellipsis={{ rows: 3, expandable: true }}
+              >
+                {movies[0]?.overview}
+              </Typography.Paragraph>
+              <Typography.Title className="film-release-date" level={5}>
+                {convertDate(movies[0]?.release_date)}
+              </Typography.Title>
+            </div>
+          </div>
+        </Row>
+        <InfiniteScroll
+          dataLength={page * movies.length}
+          next={() => {
+            setPage(page + 1);
+          }}
+          hasMore={true}
+          loader={<Spinner />}
+          className="dashboard-cards"
+        >
+          <Row className="dashboard-row" gutter={[32, 16]}>
+            {movies.slice(1).map((movie: MovieModel) => (
+              <ColComponent movie={movie} />
+            ))}
+          </Row>
+        </InfiniteScroll>
+      </>
+      )
     </div>
   );
 };
